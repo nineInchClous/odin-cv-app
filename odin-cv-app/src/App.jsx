1ac9ea7b169f40ad9707a2067a51a55868bb675e
nineInchClous <thibault.castelli@pm.me>
2024-01-12 12:02:15 -0500

    Add Accordion component

--- odin-cv-app/src/App.jsx
+++ odin-cv-app/src/App.jsx
@@ -1,7 +1,15 @@
 import './App.css';
+import Accordion from './components/Accordion';
+import Input from './components/Input';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Accordion title="Accordion test">
+        <Input id="test" label="text input" type="text" />
+      </Accordion>
+    </>
+  );
 }
 
 export default App;
---
+++ odin-cv-app/src/components/Accordion.jsx
@@ -0,0 +1,24 @@
+import '../styles/Accordion.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
+function handleClick(e) {
+  e.target.nextSibling.classList.toggle('open');
+  e.target.lastChild.firstChild.classList.toggle('open');
+}
+
+function Accordion({ title, children }) {
+  return (
+    <div className="accordion">
+      <button type="button" className="accordion-header" onClick={handleClick}>
+        <h2>{title}</h2>
+        <div className="icon">
+          <FontAwesomeIcon icon={faChevronDown} />
+        </div>
+      </button>
+      <div className="accordion-content">{children}</div>
+    </div>
+  );
+}
+
+export default Accordion;
